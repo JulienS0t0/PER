d62d1a8bd7f8759bab4b8388d250424ab16379d1
JulienS0t0 <juliensoto@free.fr>
2025-02-12 11:53:52 +0100

    fix cpu opti

--- src/operations/cpu_opti/add.c
+++ src/operations/cpu_opti/add.c
@@ -3,16 +3,35 @@
 #include <time.h>
 #include "../../matrices/matrix_utils.h"
 
+typedef enum { TYPE_INT, TYPE_FLOAT } TypeMatrice;
+
 typedef struct {
     int taille;
-    int elements[MAX_N][MAX_N];
+    TypeMatrice type;
+    union {
+        int elements_int[MAX_N][MAX_N];
+        float elements_float[MAX_N][MAX_N];
+    };
 } Matrice;
 
-void additionner_matrices(Matrice matrice1, Matrice matrice2, Matrice *resultat) {
-    resultat->taille = matrice1.taille;
-    for (int i = 0; i < matrice1.taille; i++) {
-        for (int j = 0; j < matrice1.taille; j++) {
-            resultat->elements[i][j] = matrice1.elements[i][j] + matrice2.elements[i][j];
+void additionner_matrices(const Matrice *matrice1, const Matrice *matrice2, Matrice *resultat) {
+    int taille = matrice1->taille;
+    
+    if (matrice1->type != matrice2->type) {
+        printf("Erreur : Les matrices doivent être du même type pour être additionnées.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    resultat->taille = taille;
+    resultat->type = matrice1->type;
+
+    for (int i = 0; i < taille; i++) {
+        for (int j = 0; j < taille; j++) {
+            if (matrice1->type == TYPE_INT) {
+                resultat->elements_int[i][j] = matrice1->elements_int[i][j] + matrice2->elements_int[i][j];
+            } else {
+                resultat->elements_float[i][j] = matrice1->elements_float[i][j] + matrice2->elements_float[i][j];
+            }
         }
     }
 }
@@ -27,33 +46,56 @@ int main(int argc, char *argv[]) {
     int taille1, taille2;
     void *data1 = NULL, *data2 = NULL;
     
-    int is_float = type_matrice(argv[1]) || type_matrice(argv[2]);
-    
-    charger_matrice_csv(argv[1], &data1, &taille1, is_float);
-    charger_matrice_csv(argv[2], &data2, &taille2, is_float);
+    int is_float1 = type_matrice(argv[1]);
+    int is_float2 = type_matrice(argv[2]);
+
+    if (is_float1 != is_float2) {
+        printf("Erreur : Les matrices doivent être du même type pour être additionnées.\n");
+        return EXIT_FAILURE;
+    }
+
+    charger_matrice_csv(argv[1], &data1, &taille1, is_float1);
+    charger_matrice_csv(argv[2], &data2, &taille2, is_float2);
     
     if (taille1 != taille2) {
         printf("Erreur : Les matrices doivent avoir la même taille pour être additionnées.\n");
+        free(data1);
+        free(data2);
         return EXIT_FAILURE;
     }
     
     matrice1.taille = matrice2.taille = taille1;
-    for (int i = 0; i < taille1; i++) {
-        for (int j = 0; j < taille1; j++) {
-            matrice1.elements[i][j] = ((matrix_int_type*)data1)[i * taille1 + j];
-            matrice2.elements[i][j] = ((matrix_int_type*)data2)[i * taille1 + j];
+    matrice1.type = matrice2.type = is_float1 ? TYPE_FLOAT : TYPE_INT;
+
+    if (is_float1) {
+        float *data_float1 = (float *)data1;
+        float *data_float2 = (float *)data2;
+        for (int i = 0; i < taille1; i++) {
+            for (int j = 0; j < taille1; j++) {
+                matrice1.elements_float[i][j] = data_float1[i * taille1 + j];
+                matrice2.elements_float[i][j] = data_float2[i * taille1 + j];
+            }
+        }
+    } else {
+        int *data_int1 = (int *)data1;
+        int *data_int2 = (int *)data2;
+        for (int i = 0; i < taille1; i++) {
+            for (int j = 0; j < taille1; j++) {
+                matrice1.elements_int[i][j] = data_int1[i * taille1 + j];
+                matrice2.elements_int[i][j] = data_int2[i * taille1 + j];
+            }
         }
     }
-    
+
     free(data1);
     free(data2);
     
-    // clock_t start = clock();
-    additionner_matrices(matrice1, matrice2, &resultat);
-    // clock_t end = clock();
-    
-    // double temps_execution = ((double)(end - start)) / CLOCKS_PER_SEC * 1000;
-    // printf("Addition terminée en %.2f ms.\n", temps_execution);
+    clock_t start = clock();
+    additionner_matrices(&matrice1, &matrice2, &resultat);
+    clock_t end = clock();
+
+    double temps_execution = ((double)(end - start)) / CLOCKS_PER_SEC * 1000;
+    printf("Addition terminée en %.2f ms.\n", temps_execution);
 
     return EXIT_SUCCESS;
 }
